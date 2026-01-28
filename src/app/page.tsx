58987cdc571ce13d49b45ab84269b413e428281f
nospighost <nospighost@gmail.com>
2026-01-28 07:41:47 +0100

    test

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+import Link from "next/link";
+import ProductCard from "@/components/ProductCard";
+
+
+
+export default function Home() {
+
+
+    return (
+        <main>
+            <Link href="/users">Users</Link>
+            <ProductCard />
+        </main>
+    );
+}
