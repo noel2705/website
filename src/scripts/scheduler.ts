36141cc1a701474e87d17051a253411229b29eef
nospighost <nospighost@gmail.com>
2026-02-09 12:30:20 +0100

    Save Auctions

--- src/scripts/scheduler.ts
+++ src/scripts/scheduler.ts
@@ -1,9 +1,8 @@
-// @ts-ignore
 import fetch from 'node-fetch';
 
 async function saveExpiredAuctions() {
     try {
-        const res = await fetch('http://localhost:3000/api/save-expired-auctions', {
+        const res = await fetch('http://localhost:3000/api/save-auction', {
             method: 'POST'
         });
     } catch (err) {
