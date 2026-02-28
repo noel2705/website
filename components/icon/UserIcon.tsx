4e5eaf2bd72aa6d5ea5c88a699780a90603a7f82
nospighost <noel.spieker@icloud.com>
2026-02-28 17:24:38 +0100

    Fixed UserIcon Auctions Mark List

--- components/icon/UserIcon.tsx
+++ components/icon/UserIcon.tsx
@@ -1,31 +1,25 @@
-"use client";
+'use client'
+
 import Link from "next/link";
 import {getSessionUser} from "@/hooks/useUser";
 
-export default function UserIcon({pathname}: { pathname: string }) {
-
-
-
-    const { user, loading } = getSessionUser();
-
-    if (loading) return null;
-
-    const loggedIn = !!user;
-
-    const href = loggedIn ? "/dashboard" : "/login";
+export default function UserIcon({ pathname }: { pathname: string }) {
+    const {user, loading} = getSessionUser();
+    const href = user ? "/dashboard" : "/login";
     const isActive = pathname === href || pathname.startsWith(href + "/");
-
     return (
         <>
-            <Link
-                href={href}
-                className={`link ${isActive ? "active" : ""}`}
-            >
-                {loggedIn ? "Dein Profil" : "👤"}
-            </Link>
 
 
 
-        </>
+
+    <Link
+        href={href}
+        className={`link ${isActive ? "active" : ""}`}
+    >
+        {user ? "Dein Profil" : "👤"}
+    </Link>
+
+            </>
     );
 }
